08a7331d4ba5cba036664e9cf8ca1302db92ec3b
Andrew Lee <andrew.lee.cal@gmail.com>
2011-12-27 18:04:38 -0800

    Added e4kreg.php file.

---
+++ e4kreg.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Plugin Name: E4KReg
+Plugin URI: http://e4k.berkeley.edu
+Description: Google Spreadsheets-based registration system for E4K.
+Version: 1.0
+Author: Andrew Lee
+Author URI: http://andrewlee.info
+License: GPL
+*/
+
+
+?>
